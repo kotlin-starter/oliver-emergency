78a51e4849e6e1622354b20370f6aa4313f4909f
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-10-31 15:59:16 +0900

    initialize ktor project

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "emergency"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.oliver
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
